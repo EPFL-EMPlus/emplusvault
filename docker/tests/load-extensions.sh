95d3f6c19ab4fe8c895768437009ab15e16175c5
André Rattinger <andre.rattinger@epfl.ch>
2023-05-11 14:16:59 +0200

    Docker test setup compiles pgvector

--- docker/tests/load-extensions.sh
+++ docker/tests/load-extensions.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
-
+cd /home/postgres
 git clone --branch v0.4.1 https://github.com/pgvector/pgvector.git
 cd pgvector
 make
 make install
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<EOF
-create extension pg_trgm;
-CREATE EXTENSION vector;
-select * FROM pg_extension;
-EOF
+psql --username=postgres -c "CREATE EXTENSION vector;"
