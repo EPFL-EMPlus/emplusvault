577140d2305a457886e0eee7b25c9dcf19ca8e3f
André Rattinger <andre.rattinger@epfl.ch>
2023-05-11 10:40:04 +0200

    load extension for pgvector

--- docker/tests/load-extensions.sh
+++ docker/tests/load-extensions.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+git clone --branch v0.4.1 https://github.com/pgvector/pgvector.git
+cd pgvector
+make
+make install
+
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<EOF
 create extension pg_trgm;
+CREATE EXTENSION vector;
 select * FROM pg_extension;
 EOF
