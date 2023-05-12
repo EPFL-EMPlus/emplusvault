d0bef086437d241048a917bc24757492b6c3ce33
André Rattinger <andre.rattinger@epfl.ch>
2023-05-12 16:08:24 +0200

    opted for a non async dao to make db setup and teardown in testing easier

--- rts/db/dao.py
+++ rts/db/dao.py
@@ -1,42 +1,38 @@
-import asyncpg
+import sqlalchemy
+
 
 class DataAccessObject:
     _instance = None
-    _connection_pool = None
+    _engine = None
+    _testing = False
+    _test_setup_complete = False
 
     def __new__(cls):
         if cls._instance is None:
             cls._instance = super(DataAccessObject, cls).__new__(cls)
-            cls._connection_pool = None
         return cls._instance
     
-    async def get_connection_pool(self):
-        return self._connection_pool
-    
-    async def connect(self):
-        from rts.api.server import app
-        from rts.settings import TEST_DATABASE_URL, DATABASE_URL
-        DATABASE_URL = TEST_DATABASE_URL if app.testing else DATABASE_URL
-        
-        if self._connection_pool is None:
-            self._connection_pool = await asyncpg.create_pool(DATABASE_URL)
+    def connect(self, db_url):
+        # from rts.api.server import app
+        # from rts.settings import TEST_DATABASE_URL, DATABASE_URL
+        # from rts.db import test_configure
+        # DATABASE_URL = TEST_DATABASE_URL if app.testing else DATABASE_URL
+        # self._testing = app.testing
 
-    async def fetch_one(self, query, values):
-        if self._connection_pool is None:
-            await self.connect()
-        
-        # values comes in the shape of (library_id, library_name)
-        async with self._connection_pool.acquire() as connection:
-            return await connection.fetchrow(query, *values)
-        
-    async def fetch_all(self, query, values):
-        async with self._connection_pool.acquire() as connection:
-            return await connection.fetch(query, *values)
-        
-    async def execute_query(self, query, values):
-        async with self._connection_pool.acquire() as connection:
-            return await connection.execute(query, *values)
+        # if self._testing and not self._test_setup_complete:
+        #     self._test_setup_complete = True
+        #     test_configure()
+
+        self._engine = sqlalchemy.create_engine(db_url)
+
+    def database_exists(self, db_name):
+        query = f"SELECT 1 FROM pg_database WHERE datname='{db_name}'"
+        return self._engine.execute(query).scalar() is not None
     
-    async def database_exists(self, db_name):
-        async with self._connection_pool.acquire() as connection:
-            return await connection.fetchval("SELECT EXISTS(SELECT datname FROM pg_catalog.pg_database WHERE datname = $1)", db_name)
+    def execute_query(self, query, params=None):
+        with self._engine.connect() as conn:
+            conn.execute(query, params)
+
+    def fetch_all(self, query, params=None):
+        with self._engine.connect() as conn:
+            return conn.execute(query, params).fetchall()
