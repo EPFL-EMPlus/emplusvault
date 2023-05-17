69a2583bec20847647fb056c7738651acd90001b
André Rattinger <andre.rattinger@epfl.ch>
2023-05-17 10:57:25 +0200

    moved api db calls to queries file

--- rts/api/routers/media_router.py
+++ rts/api/routers/media_router.py
@@ -3,55 +3,33 @@ from fastapi import APIRouter
 from sqlalchemy.sql import text
 from rts.api.models import Media
 from rts.db.dao import DataAccessObject
+from rts.db.queries import create_media, read_media_by_id, read_media, update_media, delete_media
 import json
 
 media_router = APIRouter()
 
 
 @media_router.post("/media/")
-async def create_media(media: Media):
-    media_data = media.dict()
-    media_data['metadata'] = json.dumps(media_data['metadata'])
-
-    query = text("""
-        INSERT INTO media (media_path, original_path, media_type, sub_type, size, metadata, library_id, hash, parent_id, start_ts, end_ts, start_frame, end_frame, frame_rate)
-        VALUES (:media_path, :original_path, :media_type, :sub_type, :size, :metadata, :library_id, :hash, :parent_id, :start_ts, :end_ts, :start_frame, :end_frame, :frame_rate)
-    """)
-    DataAccessObject().execute_query(query, media_data)
+async def create(media: Media):
+    media_data = create_media(media)
     return media_data
 
 
 @media_router.get("/media/", response_model=List[Media])
-async def read_medias():
-    query = text("SELECT * FROM media")
-    result = DataAccessObject().fetch_all(query)
-    return result
+async def get_medias():
+    return read_media()
 
 
 @media_router.get("/media/{media_id}", response_model=Media)
-async def read_media(media_id: int):
-    query = text("SELECT * FROM media WHERE media_id = :media_id")
-    result = DataAccessObject().fetch_one(query, {"media_id": media_id})
-    return result
+async def get_media(media_id: int):
+    return read_media_by_id(media_id)
 
 
 @media_router.put("/media/{media_id}")
-async def update_media(media_id: int, media: Media):
-    media_data = media.dict()
-    media_data['metadata'] = json.dumps(media_data['metadata'])
-
-    query = text("""
-        UPDATE media
-        SET media_path=:media_path, original_path=:original_path, media_type=:media_type, sub_type=:sub_type, size=:size, metadata=:metadata, library_id=:library_id, hash=:hash, parent_id=:parent_id, start_ts=:start_ts, end_ts=:end_ts, start_frame=:start_frame, end_frame=:end_frame, frame_rate=:frame_rate
-        WHERE media_id=:media_id
-    """)
-    DataAccessObject().execute_query(
-        query, {**media_data, "media_id": media_id})
-    return {**media_data, "media_id": media_id}
+async def update(media_id: int, media: Media):
+    return update_media(media_id, media)
 
 
 @media_router.delete("/media/{media_id}")
-async def delete_media(media_id: int):
-    query = text("DELETE FROM media WHERE media_id = :media_id")
-    DataAccessObject().execute_query(query, {"media_id": media_id})
-    return {"status": "Media deleted"}
+async def delete(media_id: int):
+    return delete_media(media_id)
--- rts/db/queries.py
+++ rts/db/queries.py
@@ -1,7 +1,7 @@
 from sqlalchemy.sql import text
 from rts.db.dao import DataAccessObject
 from typing import Optional
-from rts.api.models import LibraryBase, Projection
+from rts.api.models import LibraryBase, Projection, Media
 import json
 
 
@@ -70,3 +70,45 @@ def update_projection(projection_id: int, projection: Projection):
 def delete_projection(projection_id: int):
     query = text("DELETE FROM projection WHERE projection_id = :projection_id")
     DataAccessObject().execute_query(query, {"projection_id": projection_id})
+
+
+def create_media(media: Media) -> dict:
+    media_data = media.dict()
+    media_data['metadata'] = json.dumps(media_data['metadata'])
+
+    query = text("""
+        INSERT INTO media (media_path, original_path, media_type, sub_type, size, metadata, library_id, hash, parent_id, start_ts, end_ts, start_frame, end_frame, frame_rate)
+        VALUES (:media_path, :original_path, :media_type, :sub_type, :size, :metadata, :library_id, :hash, :parent_id, :start_ts, :end_ts, :start_frame, :end_frame, :frame_rate)
+    """)
+    DataAccessObject().execute_query(query, media_data)
+    return media_data
+
+
+def read_media_by_id(media_id: int) -> dict:
+    query = text("SELECT * FROM media WHERE media_id = :media_id")
+    return DataAccessObject().fetch_one(query, {"media_id": media_id})
+
+
+def read_media() -> dict:
+    query = text("SELECT * FROM media")
+    return DataAccessObject().fetch_all(query)
+
+
+def update_media(media_id: int, media: Media):
+    media_data = media.dict()
+    media_data['metadata'] = json.dumps(media_data['metadata'])
+
+    query = text("""
+        UPDATE media
+        SET media_path=:media_path, original_path=:original_path, media_type=:media_type, sub_type=:sub_type, size=:size, metadata=:metadata, library_id=:library_id, hash=:hash, parent_id=:parent_id, start_ts=:start_ts, end_ts=:end_ts, start_frame=:start_frame, end_frame=:end_frame, frame_rate=:frame_rate
+        WHERE media_id=:media_id
+    """)
+    DataAccessObject().execute_query(
+        query, {**media_data, "media_id": media_id})
+    return {**media_data, "media_id": media_id}
+
+
+def delete_media(media_id: int):
+    query = text("DELETE FROM media WHERE media_id = :media_id")
+    DataAccessObject().execute_query(query, {"media_id": media_id})
+    return {"status": "Media deleted"}
