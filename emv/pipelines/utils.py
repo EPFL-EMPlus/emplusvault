c2530621155ac4a5e69a4c029d7c11747627e32f
André Rattinger <andre.rattinger@epfl.ch>
2023-11-23 17:25:15 +0100

    Moved producer to pipeline utils function

--- emv/pipelines/utils.py
+++ emv/pipelines/utils.py
@@ -5,6 +5,8 @@ from typing import Dict, List, Optional, Tuple, Any
 import os
 from pathlib import Path
 import emv.utils
+import pika
+import json
 
 LOG = emv.utils.get_logger()
 LOG.setLevel('INFO')
@@ -16,6 +18,27 @@ TRANSCRIPT_CLIP_EXTEND_DURATION = 0
 TRANSCRIPT_CLIP_NUM_IMAGES = 3
 
 SCENE_EXPORT_NAME = 'scenes.json'
+
+def send_message(queue, data):
+    broker_url = os.getenv('BROKER_URL', 'amqp://guest:guest@rabbitmq-service:5672')
+    params = pika.URLParameters(broker_url)
+
+    connection = pika.BlockingConnection(params)
+    channel = connection.channel()
+
+    channel.queue_declare(queue=queue, durable=True)
+
+    channel.basic_publish(
+        exchange='',
+        routing_key=queue,
+        body=json.dumps(data),
+        properties=pika.BasicProperties(
+            delivery_mode=2,
+        ))
+
+    print(f"Sent: {len(data)}")
+
+    connection.close()
 
 
 def get_raw_video_audio_parts(media_folder: str) -> Tuple[str, str]:
